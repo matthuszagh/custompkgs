d61360d0ed0fa7ea1346f01d3192973f178b919c
Matt Huszagh <huszaghmatt@gmail.com>
2019-11-06 08:27:39 -0800

    circuitikz: 0.9.4 -> 0.9.5

--- pkgs/circuitikz/default.nix
+++ pkgs/circuitikz/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchFromGitHub, texlive }:
 
 stdenv.mkDerivation rec {
-  version = "v0.9.4";
+  version = "v0.9.5";
   pname = "circuitikz";
   name = "${pname}-${version}";
   tlType = "run";
@@ -10,7 +10,7 @@ stdenv.mkDerivation rec {
     owner = "circuitikz";
     repo = "circuitikz";
     rev = version;
-    sha256 = "16sxsz19i6vwnf3v4c5mlvy96zzazv2q1ahcw0sbrdb2wi7hgcx4";
+    sha256 = "0li53hbs3di1kh1bg4hm7xmp3n1ic3liifhch9cfp2hkr38qqba9";
   };
 
   dontBuild = true;
